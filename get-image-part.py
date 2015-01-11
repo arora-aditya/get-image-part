de3345888e81b904ac0d529927d0bfe5457ea49a
Patrick Lam <prof.lam@gmail.com>
2015-01-10 19:47:26 -0500

    wsgi, relative path, add better main

--- get-image-part.py
+++ get-image-part.py
@@ -4,6 +4,7 @@ import tornado.wsgi
 import io
 import time
 import random
+import os
 
 from PIL import Image
 N = 20
@@ -12,7 +13,8 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         n = int(self.get_argument("n"))
         img = int(self.get_argument("img"))
-        im = Image.open("images/"+str(img)+".jpg")
+        fn = os.path.join(os.path.dirname(__file__), "images/"+str(img)+".jpg")
+        im = Image.open(fn)
         dim = im.size
         c = im.crop((n*dim[0]/N, 0, (n+1)*dim[0]/N, dim[1]))
         bio = io.BytesIO()
@@ -21,11 +23,17 @@ class MainHandler(tornado.web.RequestHandler):
         time.sleep(abs(random.gauss(0.2, 0.2)))
         self.write(bio.getvalue())
 
-application = tornado.web.Application([
+application = tornado.wsgi.WSGIApplication([
     (r"/image", MainHandler),
 ])
 
 if __name__ == "__main__":
-    wsgi_app = tornado.wsgi.WSGIAdapter(application)
-    server = wsgiref.simple_server.make_server('', 4590, wsgi_app)
+    import logging
+    import wsgiref.simple_server
+
+    logger = logging.getLogger('tornado.application')
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+    server = wsgiref.simple_server.make_server('', 8000, application)
     server.serve_forever()
