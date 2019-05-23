d8ad4ab997ea3fc66eb1f75470aecf0e0686d7d6
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2019-05-23 14:45:34 -0400

    update to ECE252 standards + add another image - remove misc/

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-ECE 459 A1 Backend
+ECE 252 Lab 2 Backend
 ==================
 
 This is a tiny python script to return parts of an image (requires pillow, tornado).
 It simulates some service that does computation and returns after some time.
-Instead of computing, the script just waits abs(random.gauss(0.2, 0.2))
-seconds and then returns the image.
+Instead of computing, the script just waits abs(random.gauss(0.2, 0.2)) seconds and then returns the image.
 
-
+_Credit for most of the project goes to [Prof. Patrick Lam](https://github.com/patricklam) and [this repository](https://github.com/patricklam/get-image-part)_
+It has been modified to match the expected response for ECE252
--- get-image-part.py
+++ get-image-part.py
@@ -17,14 +17,14 @@ class MainHandler(tornado.web.RequestHandler):
         fn = os.path.join(os.path.dirname(__file__), "images/"+str(img)+".jpg")
         im = Image.open(fn)
         dim = im.size
-        c = im.crop((int(n*dim[0]/N), 0, int((n+1)*dim[0]/N), dim[1]))
+        c = im.crop((0, int(n*dim[1]/N), dim[0], int((n+1)*dim[1]/N)))
         c = c.convert("RGBA")
         bio = io.BytesIO()
         c.save(bio, 'PNG')
         self.set_header('Access-Control-Allow-Origin', '*')
         self.set_header('Content-Type', 'image/jpeg')
-        self.set_header('X-ECE459-Fragment', str(n))
-        # time.sleep(abs(random.gauss(0.2, 0.2)))
+        self.set_header('X-Ece252-Fragment', str(n))
+        time.sleep(abs(random.gauss(0.2, 0.2)))
         self.write(bio.getvalue())
 
 application = tornado.web.Application([
