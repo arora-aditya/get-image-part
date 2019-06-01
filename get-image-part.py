9f6c455e35056ba8293f05a5ac38f0a5bf903ad5
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2019-06-01 00:28:55 -0400

    fix issue with imgs

--- get-image-part.py
+++ get-image-part.py
@@ -14,17 +14,16 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         n = int(random.uniform(0,N))
         img = int(self.get_argument("img"))
-        fn = os.path.join(os.path.dirname(__file__), "images/"+str(img)+".jpg")
+        fn = os.path.join(os.path.dirname(__file__), "images/"+str(img)+"/output_"+str(n)+".png")
         im = Image.open(fn)
         dim = im.size
-        c = im.crop((0, int(n*dim[1]/N), dim[0], int((n+1)*dim[1]/N)))
-        c = c.convert("RGBA")
+        c = im.convert("RGBA")
         bio = io.BytesIO()
         c.save(bio, 'PNG')
         self.set_header('Access-Control-Allow-Origin', '*')
         self.set_header('Content-Type', 'image/jpeg')
         self.set_header('X-Ece252-Fragment', str(n))
-        # time.sleep(abs(random.gauss(0.2, 0.2)))
+        time.sleep(abs(random.gauss(0.2, 0.2)))
         self.write(bio.getvalue())
 
 application = tornado.web.Application([
